810ceac89a038c5bfce5aad520932a6381e36651
Alex Tu <alex.tu@canonical.com>
2021-11-14 22:26:48 +0800

    git actions: supported-gpus.json

--- get-nvidia-supported-gpus-json.sh
+++ get-nvidia-supported-gpus-json.sh
@@ -2,13 +2,13 @@
 set -ex
 #sed -i 's/^# //g'  /etc/apt/sources.list
 dis_codename="$(cat /etc/lsb-release  | grep DISTRIB_CODENAME | cut -d'=' -f2)"
-cat <<EOF >/etc/apt/sources.list.d/ubuntu-"$dis_codename"-proposed.list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/ubuntu-"$dis_codename"-proposed.list
 # Enable Ubuntu proposed archive
 deb http://archive.ubuntu.com/ubuntu/ $dis_codename-proposed restricted main multiverse universe
 deb-src http://archive.ubuntu.com/ubuntu/ $dis_codename-proposed restricted main multiverse universe
 deb-src http://archive.ubuntu.com/ubuntu/ $dis_codename-updates restricted main multiverse universe
 EOF
-apt-get update
+sudo apt-get update
 NVIDIA_DEB=$(apt-cache search  "^nvidia-driver-[0-9][0-9][0-9]" | cut -d' ' -f1 | sort -r | head -n1)
 apt-get --download-only source "$NVIDIA_DEB"
 tar xvf nvidia-graphics-drivers-*amd64.tar.gz
